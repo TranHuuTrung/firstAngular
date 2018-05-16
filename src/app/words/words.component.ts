70ef6b9b400fab8f6d37b2335eb6bcb707a14880
TranHuuTrung <tranhuutrung1408@gmail.com>
2018-05-16 10:25:51 +0700

    đảo trạng thái memorized

--- src/app/words/words.component.ts
+++ src/app/words/words.component.ts
@@ -8,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class WordsComponent implements OnInit {
 
   constructor() { }
-  
+  filterStatus = 'XEM_TAT_CA';
+  newEn = '';
+  newVn = '';
+  isShowForm = false;
   arrWords = [
     { id: 1, en: 'action', vn: 'hành động', memorized: true },
     { id: 2, en: 'actor', vn: 'diễn viên', memorized: false },
@@ -27,7 +30,37 @@ export class WordsComponent implements OnInit {
         if(this.arrWords[i].id === idReceive){
           this.arrWords[i].memorized = !this.arrWords[i].memorized;
         }
-      }
-      
+      } 
   }
+
+  addNewWord(){
+    this.arrWords.unshift({
+      id: this.arrWords.length + 1,
+      en: this.newEn,
+      vn: this.newVn,
+      memorized: false
+    });
+    this.newEn = '';
+    this.newVn = '';
+    this.isShowForm = false;
+  }
+
+  deleteWord(idDelete){
+    // for(let i=0; i< this.arrWords.length; i++){
+    //   if(this.arrWords[i].id === idDelete){
+    //     this.arrWords.splice(i,1);
+    //   }
+    // } 
+    // Cách 2 dùng hàm findIndex() của JS
+    const index = this.arrWords.findIndex(word => word.id === idDelete);
+    this.arrWords.splice(index, 1);
+
+  }
+  filters(memorized: boolean){
+    const xemTatCa = this.filterStatus === 'XEM_TAT_CA';
+    const xemDaNho = this.filterStatus === 'XEM_TU_DA_NHO' && memorized;
+    const xemChuaNho = this.filterStatus === 'XEM_TU_CHUA_NHO' && !memorized;
+    return xemTatCa || xemDaNho || xemChuaNho;
+  }
+ 
 }
